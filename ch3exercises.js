f5b72cd6c51aa5d8b8d48c47ed3ea6dda0b71ada
discolarrence <discolarrence@gmail.com>
2021-03-28 23:04:06 -0400

    finished ch3 exercises

--- ch3exercises.js
+++ ch3exercises.js
@@ -5,6 +5,30 @@ function min(a, b = 0) {
     } else if (a > b) {
         return b;
     } else {
-        return "same number";
+        return "same num";
     }
 }
+
+// Recursion
+function isEven(num) {
+    num = Math.abs(num)
+    if (num == 0) {
+        return true;
+    } else if (num == 1) {
+        return false;
+    } else {
+        return isEven(num-2);
+    }
+}
+
+//Bean Counting
+function countChar(string, letter) {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] == letter){
+            count ++;
+        }
+    }
+    return count;
+}
+
